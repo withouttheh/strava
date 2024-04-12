d83054eaa2950d136e3cd7af92f889a56863b095
Luto Sibotoboto <lutosibotoboto@gmail.com>
2024-04-12 15:46:58 +0200

    strava api done

---
+++ config/utils.py
@@ -0,0 +1,14 @@
+col_names = [
+    'id', 'achievement_count', 'athlete', 'athlete_count', 'average_speed', 'average_watts', 
+    'comment_count', 'commute', 'device_watts', 'distance', 'elapsed_time', 'elev_high', 
+    'elev_low', 'end_latlng', 'external_id', 'flagged', 'gear_id', 'has_kudoed', 'hide_from_home', 
+    'kilojoules', 'kudos_count', 'manual', 'map', 'max_speed', 'max_watts', 'moving_time', 'name', 
+    'photo_count', 'private', 'sport_type', 'start_date', 'start_date_local', 'start_latlng', 'timezone', 
+    'total_elevation_gain', 'total_photo_count', 'trainer', 'type', 'upload_id', 'upload_id_str', 
+    'weighted_average_watts', 'workout_type', 'best_efforts', 'calories', 'description', 'device_name', 
+    'embed_token', 'gear', 'laps', 'photos', 'segment_efforts', 'splits_metric', 'splits_standard', 
+    'guid', 'utc_offset', 'location_city', 'location_state', 'location_country', 'start_latitude', 
+    'start_longitude', 'pr_count', 'suffer_score', 'has_heartrate', 'average_heartrate', 'max_heartrate', 
+    'average_cadence', 'average_temp', 'instagram_primary_photo', 'partner_logo_url', 'partner_brand_tag', 
+    'from_accepted_tag', 'segment_leaderboard_opt_out', 'perceived_exertion'
+]
